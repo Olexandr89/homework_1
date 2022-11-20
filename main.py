5115b8435fbfcec0811f54f5f43bf40089a2d4b3
Olexandr89 <cadet2004@ukr.net>
2022-11-20 14:45:56 +0200

    add start file

---
+++ main.py
@@ -0,0 +1,4 @@
+import this                          #виводить на екран філософію Python від Tim Peters, який створив Zen of Python
+import antigravity                   #переходить на веб-сайт з кумедними ілюстраціями
+import __hello__                     #виводить на екран "hello world"
+from __future__ import braces        #в Python 2.6 или 2.7 дозволяє print зробити функцією, що приймає аргументи print(), як в Python 3.
